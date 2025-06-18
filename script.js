422a13f147ef6a8c791ee327073a8ca5e1beff55
Just-A-Random-Coder <okanabar@cps.edu>
2025-06-18 13:20:59 -0500

    Updated Code

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-/*
+/* 
 © 2025 Om Kanabar. All rights reserved.
 
 This source code is licensed for personal use and educational purposes only.
 Unauthorized reproduction, distribution, or commercial use is prohibited without explicit permission.
-Contact: 206570655+Just-A-Random-Coder@users.noreply.github.com  
+Contact: 206570655+Just-A-Random-Coder@users.noreply.github.com
 */
