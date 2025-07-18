f9db64efff3fbe86dc685e1bd3f672b711bf6a37
Om Kanabar <omkanabar@gmail.com>
2025-07-17 19:41:58 -0500

    Made the movement of the particles slower

--- scripts2.js
+++ scripts2.js
@@ -63,7 +63,7 @@ function initParticles() {
       },
       "move": {
         "enable": true,
-        "speed": 6,
+        "speed": 1,
         "direction": "none",
         "random": false,
         "straight": false,
